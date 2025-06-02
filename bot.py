45daddaeeff16ce363ce95a9b36dfd1f6bb70917
Ali MohammadiPanah <33523519+alimetaphor@users.noreply.github.com>
2025-06-02 22:47:53 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -67,9 +67,7 @@ class SteamBot:
                 [InlineKeyboardButton("""🧑‍🚀 پروفایل تصویری""", callback_data=f"profilepic_{steam_id}")]
             ]
 
-            caption = f"{summary['personaname']}\n
-تعداد بازی‌هات: {len(games)}n\
-لقبت: {nickname}"
+            caption = f"{summary['personaname']} تعداد بازی‌هات: {len(games)} لقبت: {nickname}"
 
             await update.message.reply_photo(
                 photo=summary["avatarfull"],
