13bcc6f451ed905a1bfe45caa70f396f0cdd9d3b
Ali MohammadiPanah <33523519+alimetaphor@users.noreply.github.com>
2025-06-02 15:46:18 +0330

    Update steam_api.py

--- steam_api.py
+++ steam_api.py
@@ -16,3 +16,7 @@ class SteamAPI:
         url = f"http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={self.api_key}&steamid={steam_id}&include_appinfo=1"
         response = requests.get(url)
         return response.json()["response"].get("games", [])
+        
+    def get_total_games(self, steam_id):
+        games = self.get_owned_games(steam_id)
+        return len(games)
